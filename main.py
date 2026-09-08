ded9ff2461755376b35b0b2c55b69a4cd76f788b
infosiddjain <infosiddjain@gmail.com>
2026-09-08 17:29:12 +0530

    feat: make a single id data api

--- main.py
+++ main.py
@@ -38,3 +38,18 @@ def get_expenses():
         "message":"Expenses fetch success",
         "expenses": expenses
     }
+    
+    
+@app.get("/expense/{expense_id}")
+def expense_one(expense_id: int):
+    
+    for data in expenses:
+        if data["id"] == expense_id:
+            return {
+                "message" :"Get expense data ",
+                "expense": data
+            }
+    
+    return {
+        "message" :"expense not found"
+    }
